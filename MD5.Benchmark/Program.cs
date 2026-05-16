1263e2b2010eacb5bc59524e42034e156d25058f
stxwright <14978758+stxwright@users.noreply.github.com>
2026-05-16 13:19:33 +0100

    ci: streamline workflow and update benchmark command

--- MD5.Benchmark/Program.cs
+++ MD5.Benchmark/Program.cs
@@ -1,25 +1,27 @@
-﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Running;
 
 namespace MD5.Benchmark
 {
     [MemoryDiagnoser]
-    public class Md5VsSha256
+    public class Md5Comparison
     {
-        private const int N = 10000;
-        private readonly byte[] data;
+        [Params(64, 1024, 1048576)]
+        public int Size { get; set; }
 
-        private readonly System.Security.Cryptography.MD5 md5 = System.Security.Cryptography.MD5.Create();
+        private byte[] data = null!;
+        private readonly System.Security.Cryptography.MD5 systemMd5 = System.Security.Cryptography.MD5.Create();
         private readonly StxWright.MD5 stxMd5 = StxWright.MD5.Create();
 
-        public Md5VsSha256()
+        [GlobalSetup]
+        public void Setup()
         {
-            data = new byte[N];
+            data = new byte[Size];
             new Random(42).NextBytes(data);
         }
 
-        [Benchmark]
-        public byte[] Md5() => md5.ComputeHash(data);
+        [Benchmark(Baseline = true)]
+        public byte[] SystemMd5() => systemMd5.ComputeHash(data);
 
         [Benchmark]
         public byte[] StxMd5() => stxMd5.ComputeHash(data);
@@ -29,7 +31,7 @@ namespace MD5.Benchmark
     {
         public static void Main(string[] args)
         {
-            var summary = BenchmarkRunner.Run<Md5VsSha256>();
+            BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
         }
     }
 }
